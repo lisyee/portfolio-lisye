303d916398081d7848b9e6e3c70c0dc5b3d9f046
lisyee <136808489+lisyee@users.noreply.github.com>
2026-05-26 22:20:00 +0800

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <section>
       <h2 className="text-2xl font-bold text-slate-800 mb-4">About Me</h2>
-      <p className="text-slate-600 leading-relaxed text-lg">
+      <p className="text-slate-600 leading-relaxed text-lg text-justify">
         High-performance Software Engineer with over 3 years of combined practical and research experience in
         Computer Vision, Robotics, and Industrial Automation. Expert in developing real-time, low-latency
         applications by translating complex MATLAB models into production-ready C++ and C# code. Proven
