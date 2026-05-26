dfa710f3fc88bfa883d0d7e200f470a9481ae51a
lisyee <136808489+lisyee@users.noreply.github.com>
2026-05-26 20:24:28 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body className="bg-slate-50 text-slate-950 antialiased">
         {children}
       </body>
